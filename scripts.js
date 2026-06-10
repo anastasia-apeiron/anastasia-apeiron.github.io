ac517029325d664ac9875af5181b325855ae27d7
anastasia-apeiron <ana.apeiron@gmail.com>
2026-06-10 18:30:59 +0200

    updated publications page

--- scripts.js
+++ scripts.js
@@ -1,6 +1,22 @@
 // Grab the eco mode switch button
 const themeToggleBtn = document.getElementById('themeToggle');
 
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
 // Listen for clicks on the button
 themeToggleBtn.addEventListener('click', () => {
     // Check if the HTML root document already has dark mode active
